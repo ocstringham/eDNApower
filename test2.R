e1985653c4b1f37afcc26e3ecd1ac9734d85d8c4
Oliver C. Stringham <oliverstringham@gmail.com>
2026-01-22 15:10:34 -0500

    Create test2.R

---
+++ test2.R
@@ -0,0 +1,62 @@
+library(eDNAoccupancy)
+library(dplyr)
+library(tidyr)
+data("fungusDetectionData")
+
+fungusDet = occData(fungusDetectionData, siteCol = "site", sampleColName = "sample")
+fit = occModel(detectionMats = fungusDet)
+posteriorSummary(fit)
+
+
+
+# Simulate survey results
+n_sites = 1 # locations
+n_samples_per_site = 30 # trees
+n_tech_reps = 3
+
+theta = runif(1000, 0.2, 0.4)  # Probability that DNA is present in a sample
+p = rbeta(1000, 5, 5)      # Probability that a technical replicate detects the DNA if it is
+# hist(rbeta(1000, 5, 5))
+
+
+# df of site, sample (1,2,3 etc), tech_rep (1,2,3 etc) where values are 0/1 for detection/non-detection
+df_list = list()
+d = 1
+for(i in 1:n_sites){
+  for(j in 1:n_samples_per_site){
+    for(k in 1:n_tech_reps){
+      # simulate detection/non-detection data
+      dna_present = rbinom(1, 1, sample(theta, size = 1))
+      detection = rbinom(1, 1, dna_present * sample(p, size = 1))
+
+      # save to list
+      df_list[[d]] = tibble(site = paste0("site_", i),
+                       sample = j,
+                       tech_rep = paste0("rep_", k),
+                       detection = detection)
+      d = d + 1
+    }
+  }
+}
+
+surveyDetData = bind_rows(df_list)
+surveyDetData = surveyDetData %>%
+  pivot_wider(names_from = tech_rep, values_from = detection) %>%
+  arrange(site, sample) %>%
+  as.data.frame()
+surveyDet = occData(surveyDetData, siteCol = "site", sampleColName = "sample")
+
+fit2 = occModel(detectionMats = surveyDet, niter = 11000, niterInterval = 5000)
+posteriorSummary(fit2, burnin = 4000, mcError = TRUE)
+
+plotTrace(fit2, paramName = "beta.(Intercept)")
+plotTrace(fit2, paramName = "alpha.(Intercept)")
+
+posteriorSummaryOfSiteOccupancy(fit2, burnin = 1000, mcError = TRUE) # psi
+posteriorSummaryOfSampleOccupancy(fit2, burnin = 1000, mcError = TRUE) # theta
+posteriorSummaryOfDetection(fit2, burnin = 1000, mcError = TRUE) # p
+
+
+
+# try msocc next and make simulated data based on N positives desired
+
